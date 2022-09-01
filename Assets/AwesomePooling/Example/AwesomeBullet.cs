1ba56e66ccca2dfd70890ebe937dea532c102f5b
DESKTOP-Q789MSK\PC <ogulcan@vertigogames.co>
2022-09-01 22:05:32 +0300

    Implemented interface to example script

--- Assets/AwesomePooling/Example/AwesomeBullet.cs
+++ Assets/AwesomePooling/Example/AwesomeBullet.cs
@@ -6,5 +6,15 @@ namespace AwesomePoolingExample
     public class AwesomeBullet : MonoBehaviour, IPoolable
     {
         public bool IsInUse { get; set; }
+
+        public void OnPooled()
+        {
+            Debug.Log(gameObject.name + " is pooled");
+        }
+
+        public void OnSelected()
+        {
+            Debug.Log(gameObject.name + " is selected from pool");
+        }
     }
 }
